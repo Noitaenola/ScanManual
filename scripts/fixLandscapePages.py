fa0010146f9dae9e46cb1b6f8618b57e0281aba2
Noita Enola <todomelobajo@yahoo.com>
2020-05-20 22:54:15 -0300

    Acomodada la redacción de la sección «6. Output» a los cambios del último commit. Corregido el script fixLandscapePages.py: debe rotar 90 grados, no 180.

--- scripts/fixLandscapePages.py
+++ scripts/fixLandscapePages.py
@@ -1,9 +1,9 @@
-# This script rotates all the landscapePages by 180 degrees.
+# This script rotates all the landscapePages by 90 degrees.
 
 import os
 
 landscapePages = [128, 161, 210, 213, 214, 224, 377, 379, 381, 382, 384, 385, 386, 387, 388, 407, 435, 438, 439, 440]
 
 for x in landscapePages:
-	os.system('mogrify -rotate 180 ../SCAN/out/SCAN_' + str(x) + '.tif')
+	os.system('mogrify -rotate 90 ../SCAN/out/SCAN_' + str(x) + '.tif')
 	print('SCAN_' + str(x) + '.tif' + ' rotated!')
